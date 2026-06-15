518e513beeacc48d016703c4044c7d5fe4ee3b38
Jannas <duvenhagejan133@gmail.com>
2026-06-15 16:46:50 +0200

    gitignore .svelte-kit

--- packages/svelte/.svelte-kit/__package__/components/TrendExport.svelte.d.ts
+++
@@ -1,8 +0,0 @@
-type $$ComponentProps = {
-    onExport: (format: 'csv' | 'json' | 'png') => void;
-    exporting?: boolean;
-};
-declare const TrendExport: import("svelte").Component<$$ComponentProps, {}, "">;
-type TrendExport = ReturnType<typeof TrendExport>;
-export default TrendExport;
-//# sourceMappingURL=TrendExport.svelte.d.ts.map
--- packages/svelte/.svelte-kit/__package__/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { default as TrendChart } from './components/TrendChart.svelte';
-export { default as TrendLegend } from './components/TrendLegend.svelte';
-export { default as TrendTimeRange } from './components/TrendTimeRange.svelte';
-export { default as TrendCursor } from './components/TrendCursor.svelte';
-export { default as TrendExport } from './components/TrendExport.svelte';
-export { TrendingEngine } from '@talos-sa/trending-core';
-export type { DataPoint, TimeSeries, TrendViewport } from '@talos-sa/trending-core';
-export type { DataQuality } from '@talos-sa/trending-core';
-//# sourceMappingURL=index.d.ts.map
